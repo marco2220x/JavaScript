c1a212dd562bccdf94f4ad2ab5d78108009fec98
marco2220x <112792541+marco2220x@users.noreply.github.com>
2022-11-06 00:19:11 -0600

    Update script.js

--- Ejercicio2/script.js
+++ Ejercicio2/script.js
@@ -1,8 +1,9 @@
+//Función que indica el evento
 function onClick() {
     console.log("hiciste clic");
     }
 
-//Declaramos una constante con el objeto obtenido a travez del dom y mandamos una
-//funcion a traves de un listener
+//Declaraación de la constante donde el objeto es el botón
 const button = document.querySelector('#boton');
+//Se llama a la función onClick una vez que ocurre un evento en el botón (se da un clic).
 button.addEventListener('click',onClick);
